3a17b689f2c6613dc7b8761ed1562ed9ba100a97
Ivan Cara <ivanpavaleoev@gmail.com>
2024-10-18 18:41:09 -0300

    adjusting cuda dependencies

--- utils/DeviceUtils.py
+++ utils/DeviceUtils.py
@@ -3,5 +3,6 @@ class DeviceUtils:
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.device_name = torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'cpu'
+        print(f'Using {self.device_name} device')
     def get_device(self):
         return self.device
